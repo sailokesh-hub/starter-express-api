06ca2aaae0355e690419263206e7b68193115146
Sai Lokesh Chirumarri <76848659+sailokesh-hub@users.noreply.github.com>
2024-02-03 17:59:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,100 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+const mysql = require('mysql2/promise');
+const port = process.env.PORT || 3001;
+
+
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://24hr7comit.site');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
+const connection = mysql.createConnection({
+  host: 'srv1327.hstgr.io',
+  user: 'u540642530_SaiLokesh',
+  password: '24Hr7@45it',
+  database: 'u540642530_User_Forms',
+});
+
+app.get("/", (req, res) => res.type('html').send(html));
+
+app.get("/get", (request, response) => {
+  response.send("you are hacked");
+});
+
+app.post('/submit-form', async (req, res) => {
+  try {
+    const formData = req.body;
+
+    // Use formData to insert data into the MySQL database
+    const [result] = await connection.execute(
+      'INSERT INTO form_data (name, email, phone, message) VALUES (?, ?, ?, ?)',
+      [formData.name, formData.email, formData.phone, formData.message]
+    );
+
+    res.status(200).json({ success: true, result });
+  } catch (error) {
+    console.error('Error handling form submission:', error);
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
+
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello from Render!</title>
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+    <script>
+      setTimeout(() => {
+        confetti({
+          particleCount: 100,
+          spread: 70,
+          origin: { y: 0.6 },
+          disableForReducedMotion: true
+        });
+      }, 500);
+    </script>
+    <style>
+      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
+      @font-face {
+        font-family: "neo-sans";
+        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+        font-style: normal;
+        font-weight: 700;
+      }
+      html {
+        font-family: neo-sans;
+        font-weight: 700;
+        font-size: calc(62rem / 16);
+      }
+      body {
+        background: white;
+      }
+      section {
+        border-radius: 1em;
+        padding: 1em;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%);
+      }
+    </style>
+  </head>
+  <body>
+    <section>
+      Hello from Render!
+    </section>
+  </body>
+</html>
+`
